2fcf962a0f880847f122b7a7a509f65410601d28
joe <joe@belgarath>
2014-01-05 23:55:52 -0800

    extensive checking makes performance bad. Need to change collision detection algo

--- Player.py
+++ Player.py
@@ -16,10 +16,9 @@ class Player:
         self.x_velo = 0
         self.y_velo = 0
         self.gravity = .5
-        self.friction = .9
+        self.friction = .90
 
         self.x_accel = 0
-        self.y_accel = 0
 
         self.moving_left = False
         self.moving_right = False
@@ -51,6 +50,9 @@ class Player:
                     break
 
             block_dist = closest_blocked_tile_row * self.tile_map.tile_height - self.y - self.height
+            # don't let player teleport to above block by sliding into a block
+            if block_dist < 0:
+                block_dist = 0
             # can move only by the minimum betwixt the distance from the player to the nearest thing
             # blocking it or the distance that it wanted to go
             self.y += min(self.y_velo, block_dist)
@@ -61,6 +63,41 @@ class Player:
         ## check for collision with respect to the x direction
         self.x_velo += self.x_accel
         self.x_velo *= self.friction
+
+        cur_tile_col = int(self.x // self.tile_map.tile_width)
+        top_line_row = int(self.y // self.tile_map.tile_height)
+        bot_line_row = int((self.y + self.height) // self.tile_map.tile_height)
+        closest_blocked_tile_col = -1
+        # going right
+        '''
+        if self.x_velo > 0:
+            for col in range(cur_tile_col, self.tile_map.num_cols):
+                for row in range(top_line_row, bot_line_row):
+                    if self.tile_map.tiles[row][col] == 0:
+                        closest_blocked_tile_col = col
+                        break
+                if closest_blocked_tile_col != -1:
+                    break
+
+            block_dist = closest_blocked_tile_col * self.tile_map.tile_width - self.x - self.width
+            if block_dist < 0:
+                block_dist = 0
+            self.x += min(self.x_velo, block_dist)
+
+        # going left
+        elif self.x_velo < 0:
+            for col in range(cur_tile_col, -1, -1):
+                for row in range(top_line_row, bot_line_row + 1):
+                    if self.tile_map.tiles[row][col] == 0:
+                        closest_blocked_tile_col = col
+                        break
+                if closest_blocked_tile_col != -1:
+                    break
+            block_dist = (1 + closest_blocked_tile_col) * self.tile_map.tile_width - self.x
+            if block_dist == 0:
+                self.x_velo = 0
+            self.x -= min(abs(self.x_velo), abs(block_dist))
+        '''
         self.x += self.x_velo
 
 
