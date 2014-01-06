578096167897277e9f3730d91777e155651a8e45
joe <joe@belgarath>
2014-01-05 19:29:24 -0800

    added simple tile class; can now draw tiles!

--- game.py
+++ game.py
@@ -1,5 +1,5 @@
 import pygame
-
+from Tile_Map import *
 
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
@@ -10,6 +10,8 @@ pygame.init()
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption("platformer")
 
+tile_map = TileMap()
+
 done = False
 clock = pygame.time.Clock()
 
@@ -19,7 +21,17 @@ while not done:
             done = True
 
     screen.fill(BLACK)
-    pygame.draw.rect(screen, WHITE, [0, 0, 40, 40])
+    for row in range(tile_map.num_rows):
+        for col in range(tile_map.num_cols):
+            cur_tile = tile_map.tiles[row][col]
+            if cur_tile == 0:
+                cur_color = BLACK
+            elif cur_tile == 1:
+                cur_color = WHITE
+            pygame.draw.rect(screen, cur_color, [tile_map.tile_width * col,
+                                                 tile_map.tile_height * row,
+                                                 tile_map.tile_width,
+                                                 tile_map.tile_height])
 
     pygame.display.flip()
     clock.tick(FPS)
